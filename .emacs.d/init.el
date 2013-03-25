a1feb2e18f6877ac994e396dad8243de8a526388
ISHII Masaru <noitorai555@gmail.com>
2013-03-25 22:22:38 +0900

    delete trailing whitespace

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,7 +8,7 @@
 
 (require 'auto-install)
 
-;; add auto-install elisps to load-path 
+;; add auto-install elisps to load-path
 (add-to-list 'load-path auto-install-directory)
 
 ;; install-elisp.el compatibility mode
@@ -36,7 +36,7 @@
                     'japanese-jisx0208
                     '("IPAゴシック" . "unicode-bmp")))))
 
-;; 列数表示                                                                     
+;; 列数表示
 (column-number-mode t)
 
 ;;リージョンに色を付ける
@@ -53,32 +53,32 @@
 (global-set-key "\C-c\C-l" 'toggle-truncate-lines)
 
 ;; FIXME elscreen と合わせて要不要を検討(使用するなら Action へ移動
-;; ;;C-zをtermに強奪されてないようにする                                   
-;; (add-hook 'term-mode-hook '(lambda ()                    
-;;                              (define-key term-raw-map "\C-z"      
+;; ;;C-zをtermに強奪されてないようにする
+;; (add-hook 'term-mode-hook '(lambda ()
+;;                              (define-key term-raw-map "\C-z"
 ;;                                (lookup-key (current-global-map) "\C-z"))))
 
 ;; TODO: 導入を検討 jaspace-mode
 ;; ;; 切り替えは M-x jaspace-mode-{on,off}
 ;; (require 'jaspace)
-;; ;; 全角空白を表示させる。                                                       
+;; ;; 全角空白を表示させる。
 ;; (setq jaspace-alternate-jaspace-string "□ ")
-;; ;; 改行記号を表示させる。                                                       
+;; ;; 改行記号を表示させる。
 ;; (setq jaspace-alternate-eol-string "↓ \n")
-;; ;; タブ記号を表示。                                                             
-;; (setq jaspace-highlight-tabs t)  ; highlight tabs                               
+;; ;; タブ記号を表示。
+;; (setq jaspace-highlight-tabs t)  ; highlight tabs
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; action ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; C-h を Backspace に
 (global-set-key "\C-h" 'delete-backward-char)
 
-;;タブの代わりに半角スペースを使う                                              
+;;タブの代わりに半角スペースを使う
 (setq-default tab-width 4 indent-tabs-mode nil)
-;; タブストップ位置の設定                                                       
+;; タブストップ位置の設定
 (setq-default tab-stop-list
   '(4 8 12 16 20))
 
-;; 対応する括弧をハイライト                                                     
+;; 対応する括弧をハイライト
 (show-paren-mode t)
 
 ;; redo
@@ -86,7 +86,7 @@
   (define-key ctl-x-map (if window-system "U" "r") 'redo)
   (define-key global-map [?\C-.] 'redo))
 
-;; ビープ音を画面のフラッシュに変更                                             
+;; ビープ音を画面のフラッシュに変更
 (setq visible-bell t)
 
 ;; Other
