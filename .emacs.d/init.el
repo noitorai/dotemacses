45a5dbb6f60479158716c6188ac38c1aa352a7ab
ISHII Masaru <noitorai555@gmail.com>
2014-12-20 09:16:12 +0900

    Compatibility fix for emacs 24.4, but not finished yet
      - enabled byte compile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,12 +18,11 @@
 ;; gather ediff buffers together in one frame
 (defvar ediff-window-setup-function 'ediff-setup-windows-plain)
 
-
-;; ;;;; byte compile
-;; (require 'auto-async-byte-compile)
-;; ;; ignore pattern
-;; (setq auto-async-byte-compile-exclude-files-regexp "/junk/")
-;; (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
+;;;; byte compile
+(require 'auto-async-byte-compile)
+;; ignore pattern
+(setq auto-async-byte-compile-exclude-files-regexp "/junk/")
+(add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode)
 
 ;;; package.el
 ;; Installation for Emacs24:
